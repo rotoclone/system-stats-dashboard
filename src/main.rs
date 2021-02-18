072f9ecdbae1fd4fa28e5317cd8c49096752a528
rotoclone <rotoclone@gmail.com>
2021-02-17 18:27:37 -0600

    Reorder response

--- src/main.rs
+++ src/main.rs
@@ -13,86 +13,49 @@ const BYTES_PER_MB: u64 = 1_000_000;
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 struct Stats {
-    /// Stats for each mounted filesystem
-    filesystems: Vec<MountStats>,
-    /// Memory stats
-    memory: MemoryStats,
-    /// CPU stats
-    cpu: CpuStats,
     /// General system stats
     general: GeneralStats,
+    /// CPU stats
+    cpu: CpuStats,
+    /// Memory stats
+    memory: MemoryStats,
+    /// Stats for each mounted filesystem
+    filesystems: Vec<MountStats>,
 }
 
-/// Stats for a mounted filesystem
+/// General system stats
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
-struct MountStats {
-    /// Type of filesystem (NTFS, ext3, etc.)
-    fs_type: String,
-    /// Name of the device corresponding to this mount
-    mounted_from: String,
-    /// Root path corresponding to this mount
-    mounted_on: String,
-    /// Space of this mount used in megabytes
-    used_mb: u64,
-    /// Total space for this mount in megabytes
-    total_mb: u64,
+struct GeneralStats {
+    /// Number of seconds the system has been running
+    uptime_seconds: u64,
+    /// One, five, and fifteen-minute load average values for the system
+    load_averages: [f32; 3],
 }
 
-impl MountStats {
-    /// Gets a list of mount stats for the provided system. Only mounts with more than 0 bytes of total space are included.
-    fn from(sys: &System) -> Vec<MountStats> {
-        match sys.mounts() {
-            Ok(mounts) => mounts
-                .into_iter()
-                .filter_map(|mount| {
-                    if mount.total.as_u64() == 0 {
-                        None
-                    } else {
-                        let used = saturating_sub_bytes(mount.total, mount.avail);
-                        Some(MountStats {
-                            fs_type: mount.fs_type,
-                            mounted_from: mount.fs_mounted_from,
-                            mounted_on: mount.fs_mounted_on,
-                            used_mb: bytes_to_mb(used),
-                            total_mb: bytes_to_mb(mount.total),
-                        })
-                    }
-                })
-                .collect(),
+impl GeneralStats {
+    /// Gets general stats for the provided system.
+    fn from(sys: &System) -> GeneralStats {
+        let uptime_seconds = match sys.uptime() {
+            Ok(x) => x.as_secs(),
             Err(e) => {
-                log("Error getting mounts: ", e);
-                Vec::new()
-            }
-        }
-    }
-}
-
-/// Memory stats
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
-struct MemoryStats {
-    /// Megabytes of memory used
-    used_mb: u64,
-    /// Megabytes of memory total
-    total_mb: u64,
-}
-
-impl MemoryStats {
-    /// Gets memory stats for the provided system.
-    fn from(sys: &System) -> MemoryStats {
-        let (used_mb, total_mb) = match sys.memory() {
-            Ok(mem) => {
-                let used_mem = saturating_sub_bytes(mem.total, mem.free);
-                (bytes_to_mb(used_mem), bytes_to_mb(mem.total))
-            }
-            Err(e) => {
-                log("Error getting memory usage: ", e);
-                (0, 0)
+                error!("Error getting uptime: {}", e);
+                0
             }
         };
 
-        MemoryStats { used_mb, total_mb }
+        let load_averages = match sys.load_average() {
+            Ok(x) => [x.one, x.five, x.fifteen],
+            Err(e) => {
+                log("Error getting load average: ", e);
+                [0.0, 0.0, 0.0]
+            }
+        };
+
+        GeneralStats {
+            uptime_seconds,
+            load_averages,
+        }
     }
 }
 
@@ -155,38 +118,75 @@ impl CpuStats {
     }
 }
 
-/// General system stats
+/// Memory stats
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
-struct GeneralStats {
-    /// Number of seconds the system has been running
-    uptime_seconds: u64,
-    /// One, five, and fifteen-minute load average values for the system
-    load_averages: [f32; 3],
+struct MemoryStats {
+    /// Megabytes of memory used
+    used_mb: u64,
+    /// Megabytes of memory total
+    total_mb: u64,
 }
 
-impl GeneralStats {
-    /// Gets general stats for the provided system.
-    fn from(sys: &System) -> GeneralStats {
-        let uptime_seconds = match sys.uptime() {
-            Ok(x) => x.as_secs(),
+impl MemoryStats {
+    /// Gets memory stats for the provided system.
+    fn from(sys: &System) -> MemoryStats {
+        let (used_mb, total_mb) = match sys.memory() {
+            Ok(mem) => {
+                let used_mem = saturating_sub_bytes(mem.total, mem.free);
+                (bytes_to_mb(used_mem), bytes_to_mb(mem.total))
+            }
             Err(e) => {
-                error!("Error getting uptime: {}", e);
-                0
+                log("Error getting memory usage: ", e);
+                (0, 0)
             }
         };
 
-        let load_averages = match sys.load_average() {
-            Ok(x) => [x.one, x.five, x.fifteen],
-            Err(e) => {
-                log("Error getting load average: ", e);
-                [0.0, 0.0, 0.0]
-            }
-        };
+        MemoryStats { used_mb, total_mb }
+    }
+}
 
-        GeneralStats {
-            uptime_seconds,
-            load_averages,
+/// Stats for a mounted filesystem
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct MountStats {
+    /// Type of filesystem (NTFS, ext3, etc.)
+    fs_type: String,
+    /// Name of the device corresponding to this mount
+    mounted_from: String,
+    /// Root path corresponding to this mount
+    mounted_on: String,
+    /// Space of this mount used in megabytes
+    used_mb: u64,
+    /// Total space for this mount in megabytes
+    total_mb: u64,
+}
+
+impl MountStats {
+    /// Gets a list of mount stats for the provided system. Only mounts with more than 0 bytes of total space are included.
+    fn from(sys: &System) -> Vec<MountStats> {
+        match sys.mounts() {
+            Ok(mounts) => mounts
+                .into_iter()
+                .filter_map(|mount| {
+                    if mount.total.as_u64() == 0 {
+                        None
+                    } else {
+                        let used = saturating_sub_bytes(mount.total, mount.avail);
+                        Some(MountStats {
+                            fs_type: mount.fs_type,
+                            mounted_from: mount.fs_mounted_from,
+                            mounted_on: mount.fs_mounted_on,
+                            used_mb: bytes_to_mb(used),
+                            total_mb: bytes_to_mb(mount.total),
+                        })
+                    }
+                })
+                .collect(),
+            Err(e) => {
+                log("Error getting mounts: ", e);
+                Vec::new()
+            }
         }
     }
 }
@@ -206,10 +206,10 @@ fn stats() -> Json<Stats> {
     let sys = System::new();
 
     Json(Stats {
-        filesystems: MountStats::from(&sys),
-        memory: MemoryStats::from(&sys),
-        cpu: CpuStats::from(&sys, Duration::from_millis(200)),
         general: GeneralStats::from(&sys),
+        cpu: CpuStats::from(&sys, Duration::from_millis(200)),
+        memory: MemoryStats::from(&sys),
+        filesystems: MountStats::from(&sys),
     })
 }
 
