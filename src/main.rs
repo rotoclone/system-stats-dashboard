1709f7f009eff09f056756ad7be85f4d13181ce3
rotoclone <48834501+rotoclone@users.noreply.github.com>
2021-02-21 22:56:57 -0600

    Update stats in the background and maintain stats history (#1)
    * Add StatsHistory
    
    * Use a vector instead
    
    * Make it work
    
    * Minor changes to raspberry pi cross-compile job
    
    * Don't run audit for every push and PR

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,62 @@
-use rocket::http::Status;
+use rocket::{http::Status, State};
 use rocket_contrib::json::Json;
 use systemstat::{Duration, Platform, System};
 
 mod stats;
 use stats::*;
 
+mod stats_history;
+use stats_history::*;
+
 #[macro_use]
 extern crate rocket;
 
-const DEFAULT_CPU_LOAD_SAMPLE_DURATION: Duration = Duration::from_millis(250);
-const MAX_CPU_LOAD_SAMPLE_MS: u16 = 1000;
+const STATS_HISTORY_SIZE: usize = 100;
+const STATS_UPDATE_FREQUENCY: Duration = Duration::from_secs(3);
+const CPU_LOAD_SAMPLE_DURATION: Duration = Duration::from_millis(250);
 
 /// Endpoint to get all the system stats.
-#[get("/stats?<cpu_sample_ms>")]
-fn get_all_stats(cpu_sample_ms: Option<u16>) -> Result<Json<AllStats>, Status> {
-    //TODO use a custom request guard instead?
-    let cpu_sample_duration = parse_cpu_sample_ms(cpu_sample_ms)?;
-    let sys = System::new();
-
-    Ok(Json(AllStats {
-        general: GeneralStats::from(&sys),
-        cpu: CpuStats::from(&sys, cpu_sample_duration),
-        memory: MemoryStats::from(&sys),
-        filesystems: MountStats::from(&sys),
-        network: NetworkStats::from(&sys),
-    }))
+#[get("/stats")]
+fn get_all_stats(stats_history: State<UpdatingStatsHistory>) -> Result<Json<AllStats>, Status> {
+    match stats_history
+        .stats_history
+        .lock()
+        .unwrap()
+        .get_most_recent_stats()
+    {
+        Some(x) => Ok(Json((*x).clone())),
+        None => Err(Status::InternalServerError),
+    }
 }
 
 /// Endpoint to get general stats.
 #[get("/stats/general")]
-fn get_general_stats() -> Json<GeneralStats> {
-    Json(GeneralStats::from(&System::new()))
+fn get_general_stats(
+    stats_history: State<UpdatingStatsHistory>,
+) -> Result<Json<GeneralStats>, Status> {
+    match stats_history
+        .stats_history
+        .lock()
+        .unwrap()
+        .get_most_recent_stats()
+    {
+        Some(x) => Ok(Json((*x).general.clone())),
+        None => Err(Status::InternalServerError),
+    }
 }
 
 /// Endpoint to get CPU stats.
-#[get("/stats/cpu?<cpu_sample_ms>")]
-fn get_cpu_stats(cpu_sample_ms: Option<u16>) -> Result<Json<CpuStats>, Status> {
-    //TODO use a custom request guard instead?
-    let cpu_sample_duration = parse_cpu_sample_ms(cpu_sample_ms)?;
-    Ok(Json(CpuStats::from(&System::new(), cpu_sample_duration)))
+#[get("/stats/cpu")]
+fn get_cpu_stats(stats_history: State<UpdatingStatsHistory>) -> Result<Json<CpuStats>, Status> {
+    match stats_history
+        .stats_history
+        .lock()
+        .unwrap()
+        .get_most_recent_stats()
+    {
+        Some(x) => Ok(Json((*x).cpu.clone())),
+        None => Err(Status::InternalServerError),
+    }
 }
 
 /// Endpoint to get memory stats.
@@ -67,33 +85,22 @@ fn get_network_stats() -> Json<NetworkStats> {
 
 #[launch]
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount(
-        "/",
-        routes![
-            get_all_stats,
-            get_general_stats,
-            get_cpu_stats,
-            get_memory_stats,
-            get_filesystem_stats,
-            get_network_stats,
-        ],
-    )
-}
-
-/// Parses the provided CPU sample milleseconds into a `Duration`.
-///
-/// If `cpu_sample_ms` is `None`, `DEFAULT_CPU_LOAD_SAMPLE_DURATION` will be returned.
-/// If `cpu_sample_ms` is greater than `MAX_CPU_LOAD_SAMPLE_MS`, `Err` will be returned.
-/// Otherwise, a `Duration` built from `cpu_sample_ms` will be returned.
-fn parse_cpu_sample_ms(cpu_sample_ms: Option<u16>) -> Result<Duration, Status> {
-    match cpu_sample_ms {
-        Some(x) => {
-            if x > MAX_CPU_LOAD_SAMPLE_MS {
-                Err(Status::BadRequest)
-            } else {
-                Ok(Duration::from_millis(x.into()))
-            }
-        }
-        None => Ok(DEFAULT_CPU_LOAD_SAMPLE_DURATION),
-    }
+    rocket::ignite()
+        .mount(
+            "/",
+            routes![
+                get_all_stats,
+                get_general_stats,
+                get_cpu_stats,
+                get_memory_stats,
+                get_filesystem_stats,
+                get_network_stats,
+            ],
+        )
+        .manage(UpdatingStatsHistory::new(
+            System::new(),
+            CPU_LOAD_SAMPLE_DURATION,
+            STATS_UPDATE_FREQUENCY,
+            StatsHistory::new(STATS_HISTORY_SIZE),
+        ))
 }
---
+++ src/stats_history.rs
@@ -0,0 +1,147 @@
+use systemstat::System;
+use thread::JoinHandle;
+
+use crate::stats::AllStats;
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+
+/// Stats history that updates itself periodically.
+pub struct UpdatingStatsHistory {
+    /// The thread that handles updating the stats.
+    _update_thread: JoinHandle<()>,
+    /// The stats history.
+    pub stats_history: Arc<Mutex<StatsHistory>>,
+}
+
+impl UpdatingStatsHistory {
+    /// Creates an `UpdatingStatsHistory`.
+    /// # Params
+    /// * `system` - The system to gather stats from.
+    /// * `cpu_sample_duration` - The amount of time to take to sample CPU load.
+    /// * `update_frequency` - How often new stats should be gathered.
+    /// * `stats_history` - The stats history to update.
+    pub fn new(
+        system: System,
+        cpu_sample_duration: Duration,
+        update_frequency: Duration,
+        stats_history: StatsHistory,
+    ) -> UpdatingStatsHistory {
+        let shared_stats_history = Arc::new(Mutex::new(stats_history));
+        let update_thread_stats_history = Arc::clone(&shared_stats_history);
+        let update_thread = thread::spawn(move || loop {
+            let new_stats = AllStats::from(&system, cpu_sample_duration);
+            {
+                // This needs to be in its own block so the mutex is unlocked before the thread::sleep
+                let mut history = update_thread_stats_history.lock().unwrap();
+                history.push(new_stats);
+            }
+            thread::sleep(update_frequency);
+        });
+
+        UpdatingStatsHistory {
+            _update_thread: update_thread,
+            stats_history: shared_stats_history,
+        }
+    }
+}
+
+/// A rolling history of system stats. As new stats are added, the oldest stats will be replaced if the history is full.
+pub struct StatsHistory {
+    /// The maximum size of the stats list.
+    max_size: usize,
+    /// The list of stats.
+    stats: Vec<AllStats>,
+    /// The index of the most recently added stats.
+    most_recent_index: usize,
+}
+
+impl StatsHistory {
+    /// Creates a `StatsHistory`.
+    /// # Params
+    /// * `max_size` - The maximum number of entries to hold in this history.
+    pub fn new(max_size: usize) -> StatsHistory {
+        StatsHistory {
+            max_size,
+            stats: Vec::with_capacity(max_size),
+            most_recent_index: 0,
+        }
+    }
+
+    /// Adds stats to the history.
+    /// # Params
+    /// * `new_stats` - The stats to add.
+    fn push(&mut self, new_stats: AllStats) {
+        if self.stats.len() == self.max_size {
+            // The list is full, so we need to replace an existing entry
+            self.most_recent_index = self.get_next_index();
+            self.stats[self.most_recent_index] = new_stats;
+        } else {
+            // The list isn't full yet, so we can just add a new entry to the end
+            self.stats.push(new_stats);
+            self.most_recent_index = self.stats.len() - 1;
+        }
+    }
+
+    /// Gets the most recently added stats from the history. Returns `None` if the history is empty.
+    pub fn get_most_recent_stats(&self) -> Option<&AllStats> {
+        if self.stats.is_empty() {
+            None
+        } else {
+            Some(&self.stats[self.most_recent_index])
+        }
+    }
+
+    fn get_next_index(&self) -> usize {
+        (self.most_recent_index + 1) % (self.max_size - 1)
+    }
+}
+
+impl<'a> IntoIterator for &'a StatsHistory {
+    type Item = &'a AllStats;
+    type IntoIter = StatsHistoryIterator<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let starting_index = match self.stats.last() {
+            // The array is full, so the oldest stats are in the next index. (Since it wraps around)
+            Some(_) => self.get_next_index(),
+            // The array is not full, so the oldest stats are at the beginning of the array.
+            None => 0,
+        };
+
+        StatsHistoryIterator {
+            stats_history: self,
+            index: starting_index,
+            done: false,
+        }
+    }
+}
+
+pub struct StatsHistoryIterator<'a> {
+    stats_history: &'a StatsHistory,
+    index: usize,
+    done: bool,
+}
+
+impl<'a> Iterator for StatsHistoryIterator<'a> {
+    type Item = &'a AllStats;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done {
+            return None;
+        }
+
+        let result = &self.stats_history.stats[self.index];
+        if self.index == self.stats_history.most_recent_index {
+            self.done = true;
+        } else {
+            self.index = (self.index + 1) % (self.stats_history.max_size - 1);
+        }
+
+        Some(result)
+    }
+}
+
+//TODO tests
